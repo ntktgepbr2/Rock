fa1acb5ee91ab38e7f5c417000526eca13de7568
Daniil Kryshtofik <playmeharder@gmail.com>
2021-05-28 14:16:13 +0300

    init commit

---
+++ Rock/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rock.Models;
+
+namespace Rock.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
